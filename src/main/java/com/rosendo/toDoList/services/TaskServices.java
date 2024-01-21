1d54f176b84cc81e9c3eb00f195a4c35aa1daa40
Paulo Vitor Rosendo <paulo.vitor.rsd@poli.ufrj.br>
2024-01-21 01:00:29 -0300

    creating the services

--- src/main/java/com/rosendo/toDoList/services/TaskServices.java
+++ src/main/java/com/rosendo/toDoList/services/TaskServices.java
@@ -1,4 +1,58 @@
 package com.rosendo.toDoList.services;
 
+import com.rosendo.toDoList.dtos.TaskRecordDto;
+import com.rosendo.toDoList.models.TaskModel;
+import com.rosendo.toDoList.repositories.TaskRepository;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class TaskServices {
+
+    @Autowired
+    TaskRepository repository;
+
+    public Optional<TaskModel> getTaskById(UUID id){
+        return repository.findById(id);
+    }
+    
+    public List<TaskModel> getAllTasks(){
+        return repository.findAll();
+    }
+
+    public TaskModel createTask(TaskRecordDto modelDto){
+        var model = new TaskModel();
+        BeanUtils.copyProperties(modelDto, model);
+
+        return repository.save(model);
+    }
+
+    public TaskModel updateTask(UUID id, TaskRecordDto modelDto){
+
+        Optional<TaskModel> task = repository.findById(id);
+
+        if(task.isEmpty()){} //TODO adicionar a exceção
+        
+        var model = task.get();
+        
+        BeanUtils.copyProperties(modelDto, model);
+
+        return repository.save(model);
+    }
+    
+    public void deleteTask(UUID id){
+
+        Optional<TaskModel> task = repository.findById(id);
+
+        if(task.isEmpty()){} //TODO adicionar a exceção
+
+        repository.delete(task.get());
+    }
+
 }
